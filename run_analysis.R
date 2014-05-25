3896aead48bf7dd6bc461edfd2d33da18dc708ce
yue.liu <yue.liu@qunar.com>
2014-05-25 18:19:20 +0800

    add script env

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,5 @@
+#!/usr/bin/Rscript
+
 #download the zip file from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
 if(!file.exists("~/data")){
   dir.create("~/data")
